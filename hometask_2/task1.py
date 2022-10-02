e187b7b7cc5f973a2f1f858d0bac6add4d67a6db
alexey <alexm171002@mail.ru>
2022-10-02 06:36:26 +0300

    task 1 and 2 are ready

---
+++ hometask_2/task1.py
@@ -0,0 +1,16 @@
+a = float(input("Введите число"))
+from math import floor
+sum = 0
+
+while (a-floor(a) > 0):
+    a = a*10
+print(a)
+while (a>10):
+    b = a%10
+    a = floor(a/10)
+    sum = sum+b
+sum = sum + a
+
+
+print(sum)
+print(0.56*100)
---
+++ hometask_2/task2.py
@@ -0,0 +1,8 @@
+a = int(input('Введите число'))
+sum = a
+
+while (a > 1):
+    a = a-1
+    sum = sum*a
+    
+print(sum)
